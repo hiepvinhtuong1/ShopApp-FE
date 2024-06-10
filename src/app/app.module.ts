311da91c65c7b7e20b275d4dff57ddd8ae95c660
hiep <buituanhiepvinhtuong@gmail.com>
2024-06-10 11:20:53 +0700

    Done frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FooterComponent } from './footer/footer.component';
 import { OrderComponent } from './order/order.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 
 @NgModule({
@@ -15,17 +17,21 @@ import { LoginComponent } from './login/login.component';
     FooterComponent,
     OrderComponent,
     OrderConfirmComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [
-    HomeComponent,
+    //HomeComponent,
+    DetailProductComponent
     //OrderComponent,
     //OrderConfirmComponent,
     //LoginComponent,
+    //RegisterComponent
   ]
 })
 export class AppModule { }
